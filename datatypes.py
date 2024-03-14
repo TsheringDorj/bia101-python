c9be2d3196b603260b06f5ba88922bc4634d190e
Tshering Dorji <147790493+TsheringDorj@users.noreply.github.com>
2024-03-14 14:23:50 +0600

    data types

---
+++ datatypes.py
@@ -0,0 +1,40 @@
+#creation of array
+array1 = [1,2,3,"Thimphu",2.5]
+
+
+#retriving
+element1 = array1[0]
+element2 = array1[4]
+lastElement = array1[-5]
+
+#modifying elements
+array1[0] = 10
+
+
+#geting no. of element in an array
+no_of_elements = len(array1)
+
+
+#slicing
+elements = array1[0:3]
+#print(elements)
+
+#print elements
+
+arr1 = [1,10]
+arr2 = ['thimphu', 'mongar']
+number_to_check = 1
+result = number_to_check in arr1
+
+#print(result)
+
+arrVariable = [1,2,3]
+arrVariable.append(4)
+#print(arrVariable)
+
+#insert at a specific index
+arrVariable.insert(1,10)
+print(arrVariable)
+arrVariable.sort()
+print(arrVariable)
+
